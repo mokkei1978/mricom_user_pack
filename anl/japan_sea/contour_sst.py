79adbfe214a93d90803e5798ebb404614f17ceaa
Kei Sakamoto <sakamoto.kei.6f@kyoto-u.ac.jp>
2025-06-10 16:42:42 +0900

    日本海のMGDSST平年値を図示する refs #5

--- anl/japan_sea/contour_sst.py
+++ anl/japan_sea/contour_sst.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """SST分布を描く
 
-Usage: contour_sst.py YMD
+Usage: contour_sst.py NDATA YMD
 
 Arguments:
-  YMD  date for plot(YYYY-MM-DD)
+  NDATA date number (see data.py)
+  YMD   date for plot(YYYY-MM-DD)
 """
 
 import sys
@@ -28,37 +29,41 @@ logger = logging.getLogger(__name__)
 #logger.info('START')
 
 args = docopt(__doc__)
+ndata = int( args.get('NDATA') )
 date = args.get('YMD')
 logger.debug(date)
 
-conf=confs[0]
+conf=confs[ndata]
 
 DS = xarray_maker.open_dataset(conf["file"],conf['kind'])
 logger.debug(DS)
 
 da = DS["thetao"].sel(time=date).squeeze()
-da = da.where( da != 999. )
+undef = conf.get('undef',0.)
+if undef != 0. :
+    da = da.where( da != undef )
 
 fig = plt.figure()
 ax = plt.subplot(1,1,1,projection=ccrs.PlateCarree(central_longitude=0) )
 proj = ccrs.PlateCarree()
-ax.set_extent((127., 143., 33., 50.), crs=proj )
+
+ax.set_xticks( np.arange(130.,140.1,5.), crs=proj )
+ax.set_yticks( np.arange(35.,50.1,5.), crs=proj )
+ax.set_extent( (127., 143., 33., 50.), crs=proj )
 
 da.plot.pcolormesh( transform=proj,
                     cmap=cm.jet, levels=np.arange(-1.,30.1,1.) )
 
 cntr = da.plot.contour(transform=proj,levels=20, colors="black" )
+ax.clabel(cntr)
 
 ax.coastlines()
-ax.clabel(cntr)
-ax.set_xticks( np.arange(130.,140.1,5.), crs=proj )
-ax.set_yticks( np.arange(35.,50.1,5.), crs=proj )
 ax.xaxis.set_major_formatter( LongitudeFormatter(zero_direction_label=True) )
 ax.yaxis.set_major_formatter( LatitudeFormatter() )
 ax.set_title( conf["name"]+' '+date )
 ax.set_xlabel('')
 ax.set_ylabel('')
 
+plt.savefig('temp.png', bbox_inches='tight')
 plt.show()
-#plt.savefig('temp.png', bbox_inches='tight')
 #logger.info('OUTPUT: temp.png')
--- anl/japan_sea/data.py
+++ anl/japan_sea/data.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
 confs = [ { 'name':'HIMSST',
-            'file':'../../link/data/netCDF/HIMSST/2025/nc_sst.2*',
+            'file':'../../link/data/netCDF/HIMSST/2024/nc_sst.2*',
+            'kind':'mricom-history',
+            'undef':999.,},
+          { 'name':'MGDSSTnorm',
+            'file':'../../link/data/netCDF/MGDSST/norm/nc_sst.2004*',
             'kind':'mricom-history',}, ]
