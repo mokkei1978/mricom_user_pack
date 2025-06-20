ae620bd87cf2d83c72def26614e16c568070afa4
Kei Sakamoto <sakamoto.kei.6f@kyoto-u.ac.jp>
2025-06-20 17:33:09 +0900

    領域平均水温の偏差の時系列を描く refs #9

--- anl/japan_sea/plot_sst_ave-anom.py
+++ anl/japan_sea/plot_sst_ave-anom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-"""SST水平平均値の時系列を描く(make_sst_ave.py の出力を使う)
+"""SST水平平均値の偏差の時系列を描く(make_sst_ave.py の出力を使う)
 
-Usage: plot_sst_ave.py NDATA
+Usage: plot_sst_ave-anom.py NDATA
 
 Arguments:
   NDATA date number (see data.py)
@@ -34,15 +34,15 @@ logger.debug(ndata)
 conf=confs[ndata]
 
 dm=xr.open_dataset('sst_ave.nc')
-#dm_norm=xr.open_dataset('sst_ave_norm.nc')
+dm_norm=xr.open_dataset('sst_ave_norm.nc')
 
 grouped=dm.groupby("time.year")
 
 colors={'2018':(0.4,0.4,1.),
      '2019':(0.5,0.5,1.),
      '2020':(0.6,0.6,1.),
-#     '2021':(0.7,0.7,1.),
-     '2021':'yellow',
+     '2021':(0.7,0.7,1.),
+#     '2021':'yellow',
      '2022':(0.8,0.8,1.),
      '2023':'orange',
      '2024':'red',
@@ -50,14 +50,15 @@ colors={'2018':(0.4,0.4,1.),
 
 fig, ax = plt.subplots()
 
-#dm_norm["time"]=pd.to_datetime('2020-'+dm_norm.time.dt.strftime('%m-%d').values)
-#dm_norm["thetao"].plot.line(xlim=[pd.Timestamp('2020-01-01'),pd.Timestamp('2020-12-31')],
-#                              label='norm', color='black', linewidth=2 )
+dm_norm["time"]=pd.to_datetime('2020-'+dm_norm.time.dt.strftime('%m-%d').values)
 
 for year, group in grouped:
     logger.debug(colors.get(str(year)))
     dyear = group
     dyear["time"]=pd.to_datetime('2020-'+group.time.dt.strftime('%m-%d').values)
+    dyear = dyear - dm_norm
+#    dyear = dyear.resample(time='ME').mean()
+#    dyear["time"]=pd.to_datetime('2020-'+dyear.time.dt.strftime('%m-15').values)
     dyear["thetao"].plot.line(xlim=[pd.Timestamp('2020-01-01'),pd.Timestamp('2020-12-31')],
                               label=str(year), color=colors.get(str(year)))
 
